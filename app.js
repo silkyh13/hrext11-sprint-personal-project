7ebd8ded6ad8ddddddd361ddaabfc4bce75fa958
“silkyh13” <silkyh13@gmail.com>
2019-08-23 22:52:37 -0400

    fixed the bug

--- app.js
+++ app.js
@@ -3,20 +3,13 @@ $(document).ready(() => {
     ['happiness', 10],
     ['unhappiness', 0]
   ];
-
+  //charlie = data
+  //Parse get item Charlie = data
   function render() {
     chart.load({
       columns: JSON.parse(localStorage.getItem("Charlie"))
     });
   }
-
-  $('.alive').on('mouseenter',function(){
-    // e.preventDefault();
-    $('.alive').attr('src',"Dead.png")
-  }).on('mouseleave', function() {
-    $('.alive').attr('src',"Alive.png");
-  });
-
 
   var chart = c3.generate({
     bindto: '#chart',
@@ -37,142 +30,96 @@ $(document).ready(() => {
     }
   });
 
-  var create = function(key, value) {
+  var resuscitate = function(key, value) {
     data = [
       ['happiness', 10],
       ['unhappiness', 0]
     ];
-
+    if (JSON.parse(localStorage.getItem("Charlie"))[0][1] === 0) {
+      console.log(JSON.parse(localStorage.getItem("Charlie"))[0][1])
+      alert("Charlie is dead already.")
+      return;
+    }
     localStorage.setItem("Charlie", JSON.stringify(data));
     data = JSON.parse(localStorage.getItem("Charlie"))
     render();
+
   }
-  $('.create').click(create);
+  $('.resuscitate').click(resuscitate);
 
-
-
+  //happiness <=10
   var timeIsTicking = setInterval(function() {
     // data[1][1]  = unhappiness
     var parseData = JSON.parse(localStorage.getItem("Charlie"))
-    if (parseData[1][1] !== 10) {
+    if (parseData[0][1] > 0) {
       parseData[0][1]--
       parseData[1][1]++
       localStorage.setItem("Charlie", JSON.stringify(parseData))
       chart.load({
         columns: JSON.parse(localStorage.getItem("Charlie")),
       });
-    }else
-    clearInterval(timeIsTicking);
+    }else if (parseData[1][1] ===10){
+      clearInterval(timeIsTicking);
+      $(".alive").attr("src","Dead.png");
+    }
+
   }, 2000);
 
   var encourage = function(key, value) {
     var parseData = JSON.parse(localStorage.getItem("Charlie"));
     //turn into workable object
-    parseData[0][1]++
-    parseData[1][1]--
+    if (parseData[0][1]> 0 && parseData[0][1]< 10){
+      parseData[0][1]++
+      parseData[1][1]--
+    }
     localStorage.setItem("Charlie", JSON.stringify(parseData));
-
     render();
+
   }
   $('.encourage').click(encourage);
 
-  // var get = function(key) {
-  //   return window.localStorage.getItem(key);
-  // }
+
+  var discourage= function(key, value) {
+    var parseData = JSON.parse(localStorage.getItem("Charlie"));
+    //turn into workable object
+    if (parseData[0][1]> 0 && parseData[0][1]< 10){
+      parseData[0][1]--
+      parseData[1][1]++
+    }
+    localStorage.setItem("Charlie", JSON.stringify(parseData));
+    render();
+  }
+  $('.discourage').click(discourage);
 
 
+  var resurrect = function() {
+    data = [
+      ['happiness', 10],
+      ['unhappiness', 0]
+    ];
+    localStorage.setItem("Charlie", JSON.stringify(data));
+    data = JSON.parse(localStorage.getItem("Charlie"))
+    $(".alive").attr("src", "Alive.png");
+    render();
+    var timeIsTicking = setInterval(function() {
+      var parseData = JSON.parse(localStorage.getItem("Charlie"))
+      if (parseData[0][1] <= 10 && parseData[0][1] >= 1) {
+        parseData[0][1]--
+        parseData[1][1]++
+        localStorage.setItem("Charlie", JSON.stringify(parseData))
+        chart.load({
+          columns: JSON.parse(localStorage.getItem("Charlie")),
+        });
+      } else if (parseData[1][1] === 10) {
+        clearInterval(timeIsTicking);
+        $(".alive").attr("src", "Dead.png");
+      }
+
+    }, 2000);
+  }
 
 
-
-  // var remove = function(key) {
-  //   return window.localStorage.removeItem(key);
-  // }
-
-  // var clear = function() {
-  //   return window.localStorage.clear();
-  // }
-
-  // var getItemCount = function() {
-  //   return window.localStorage.length;
-  // }
-
-  // var getKey = function(index) {
-  //   return window.localStorage.key(index);
-  // }
-
-  // var displayData = function() {
-  //   //add up all the happiness and unhappiness and add it to data
-  //   //reloads the graph
-
-  // }
-  // $(function() {
-  //   displayData();
-
-  //   $('.get').click(function() {
-  //     var key = $('#key').val();
-  //     console.log(get(key));
-  //     displayData();
-  //   })
-
-  //   $('.create').click(function() {
-  //     var key = $('#key').val();
-  //     var value = $('#value').val();
-
-  //     if (get(key) !== null) {
-  //       alert('key already exists');
-  //     } else {
-  //       console.log(create(key, value));
-  //       displayData();
-  //     }
-  //   })
-
-  //   $('.update').click(function() {
-  //     var key = $('#key').val();
-  //     var value = $('#value').val();
-
-  //     if (get(key) === null) {
-  //       alert('key does not exist');
-  //     } else {
-  //       console.log(update(key, value));
-  //       displayData();
-  //     }
-  //   })
-
-  //   $('.delete').click(function() {
-  //     var key = $('#key').val();
-
-  //     if (get(key) === null) {
-  //       alert('key does not exist');
-  //     } else {
-  //       console.log(remove(key));
-  //       displayData();
-  //     }
-  //   })
-
-  //   $('.clear').click(function() {
-  //     if (getItemCount() === 0) {
-  //       alert('nothing to clear');
-  //     } else {
-  //       console.log(clear());
-  //       displayData();
-  //     }
-  //   })
+  $('.resurrect').click(resurrect);
 
 });
-// $(‘.input’).val() 
-// JSON.stringify([{},{},{},{},]) 
-// JSON.parse(storedValue) 
 
-// .css({"background-color": generateRandomColorValue()})
-// works______________
-// $('.image-container').on('mouseenter',function(){
-//   $("div").animate({height: 'toggle'});
-// }).on('mouseleave', function() {
-//   $("div").animate({height: 'toggle'});
-// });
-
-// $('.alive').on('mouseenter',function(){
-//   $("div").animate({height: 'hide'},1000);
-// }).on('mouseleave', function() {
-//   $("div").animate({height: 'show'},1000);
-// });
